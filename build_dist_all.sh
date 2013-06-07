449d83a41685206f26c788a151db695fc42d773a
Victor Pillac <victor.pillac@nicta.com.au>
2013-06-07 12:22:55 +1000

    [Utilities] Fixed a bug in Utilities.Math.max(double[],int) Migrated to ssj-20120216
    Signed-off-by: Victor Pillac <victor.pillac@nicta.com.au>

---
+++ build_dist_all.sh
@@ -0,0 +1,24 @@
+cd VroomUtilities
+ant dist
+cd ..
+
+cd VroomModeling
+ant dist
+cd ..
+
+cd VroomHeuristics
+ant dist
+cd ..
+
+cd VroomRouting
+ant dist
+cd ..
+
+cd jMSA
+ant dist
+cd ..
+
+cd Technicians
+ant dist
+cd ..
+
